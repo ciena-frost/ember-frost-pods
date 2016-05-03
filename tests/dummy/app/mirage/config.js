b8ba44edf4a45d1613e188fe4f05c681a28a7290
Juhuan Ye <juye@ciena.com>
2016-05-03 09:59:19 -0400

    Merge with the dummy app of nms-frost-pods

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -1,3 +1,15 @@
 export default function () {
+  this.get('/dummy-records', function (db) {
+    return {
+      data: db['dummy-records'].map(attrs => {
+        return {
+          type: 'dummy-records',
+          id: attrs.id,
+          attributes: attrs
+        }
+      })
+    }
+  })
+
   this.passthrough()
 }
--- tests/dummy/app/pods/demo/route.js
+++ tests/dummy/app/pods/demo/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
+  model () {
+    return this.get('store').findAll('dummy-record')
+  },
+
+  setupController (controller, model) {
+    this._super(controller, model)
+    controller.get('podsVisibility').clear()
+//    controller.get('podsVisibility').addObject(Ember.Object.create({
+//      id: 'node',
+//      isPodVisible: false
+//    }))
+  }
 })
