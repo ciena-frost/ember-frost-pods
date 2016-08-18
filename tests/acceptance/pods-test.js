fea68c5ecfa70fdb3c2b9b9776e84e6bfab52ad9
Seena <seenarowhani95@gmail.com>
2016-08-18 09:57:43 -0400

    fixed tests

--- tests/acceptance/pods-test.js
+++ tests/acceptance/pods-test.js
@@ -4,16 +4,13 @@ import {
   it,
   beforeEach,
   afterEach
-}
-from 'mocha'
+} from 'mocha'
 import {
   expect
-}
-from 'chai'
+} from 'chai'
 import startApp from '../helpers/start-app'
 import destroyApp from '../helpers/destroy-app'
 
-/* eslint-disable */
 describe('Acceptance: PodsTest', function () {
     let application
     beforeEach(function () {
@@ -26,10 +23,10 @@ describe('Acceptance: PodsTest', function () {
 
     it('can visit /', function () {
       visit('/')
-      click('#detailPodsId')
+
       andThen(function () {
         expect(currentPath()).to.equal('demo')
       })
-    })
+
   })
-  /* eslint-enable */
+})
